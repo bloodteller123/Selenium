41f6baaba4422c25703ce28c91795868d3c40135
bloodteller123 <chenzihan708@gmail.com>
2022-11-27 18:33:21 -0500

    ATEST

--- src/test/java/crazyEight/AcceptanceTest.java
+++ src/test/java/crazyEight/AcceptanceTest.java
@@ -52,10 +52,10 @@ public class AcceptanceTest {
     }
     @AfterEach
     void teardown() {
-//        driver1.quit();
-//        driver2.quit();
-//        driver3.quit();
-//        driver4.quit();
+        driver1.quit();
+        driver2.quit();
+        driver3.quit();
+        driver4.quit();
     }
     @Test
     void row41() throws InterruptedException {
@@ -98,7 +98,7 @@ public class AcceptanceTest {
         WebElement indication4 = driver4.findElement(By.id("player_indication"));
 
         String val1 = indication1.getText();
-        System.out.println(val1);
+//        System.out.println(val1);
         assertTrue(val1.contains("right"));
 
         String val2 = indication2.getText();
@@ -178,6 +178,64 @@ public class AcceptanceTest {
         driver4.findElement(By.id("cards")).findElement(By.cssSelector("div > :first-child")).click();
 
         assertEquals(1, server.getNextPlayer());
+    }
+    @Test
+    void row46() throws InterruptedException {
+        driver1.get(sampleFile.toUri().toString());
+        driver2.get(sampleFile.toUri().toString());
+        driver3.get(sampleFile.toUri().toString());
+        driver4.get(sampleFile.toUri().toString());
+
+        driver1.findElement(By.id("startButton")).click();
+        Thread.sleep(1000);
+        js1.executeScript("document.getElementById('cards').firstElementChild.setAttribute('id', '3C')");
+        js1.executeScript("document.getElementById('cards').firstElementChild.innerHTML = '3C'");
+        js1.executeScript("document.getElementById('discard').innerHTML = '3S'");
+        js1.executeScript("cards[0] = '3C'");
+
+        driver1.findElement(By.id("cards")).findElement(By.cssSelector("div > :first-child")).click();
+        driver1.findElement(By.id("passButton")).click();
+        Thread.sleep(300);
+
+        js2.executeScript("document.getElementById('cards').firstElementChild.setAttribute('id', '3C')");
+        js2.executeScript("document.getElementById('cards').firstElementChild.innerHTML = '3C'");
+        js2.executeScript("document.getElementById('discard').innerHTML = '3S'");
+        js2.executeScript("cards[0] = '3C'");
+
+        driver2.findElement(By.id("cards")).findElement(By.cssSelector("div > :first-child")).click();
+        driver2.findElement(By.id("passButton")).click();
+        Thread.sleep(300);
+
+        js3.executeScript("document.getElementById('cards').firstElementChild.setAttribute('id', '3C')");
+        js3.executeScript("document.getElementById('cards').firstElementChild.innerHTML = '3C'");
+        js3.executeScript("document.getElementById('discard').innerHTML = '3S'");
+        js3.executeScript("cards[0] = '3C'");
+
+        driver3.findElement(By.id("cards")).findElement(By.cssSelector("div > :first-child")).click();
+        driver3.findElement(By.id("passButton")).click();
+        Thread.sleep(300);
+
+        js4.executeScript("document.getElementById('cards').firstElementChild.setAttribute('id', '1H')");
+        js4.executeScript("document.getElementById('cards').firstElementChild.innerHTML = '1H'");
+        js4.executeScript("document.getElementById('discard').innerHTML = '3H'");
+        js4.executeScript("cards[0] = '1H'");
+
+        driver4.findElement(By.id("cards")).findElement(By.cssSelector("div > :first-child")).click();
+        WebElement indication4 = driver4.findElement(By.id("player_indication"));
+
+        String val4 = indication4.getText();
+        assertTrue(val4.contains("right"));
+        assertEquals(3, server.getNextPlayer());
+        driver4.findElement(By.id("passButton")).click();
+        
+        js3.executeScript("document.getElementById('cards').firstElementChild.setAttribute('id', '7H')");
+        js3.executeScript("document.getElementById('cards').firstElementChild.innerHTML = '7H'");
+        js3.executeScript("document.getElementById('discard').innerHTML = '1H'");
+        js3.executeScript("cards[0] = '7H'");
+
+        driver3.findElement(By.id("cards")).findElement(By.cssSelector("div > :first-child")).click();
+        Thread.sleep(300);
+        assertEquals(2, server.getNextPlayer());
     }
     //js4.executeScript("document.getElementById('cards').innerHTML='';cards = [7D, JH, QH, KH, 5C]");
     //js4.executeScript("renderCards()");
